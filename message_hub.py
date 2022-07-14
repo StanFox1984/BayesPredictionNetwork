a5633bbaea61de843f1401d71b73acd12e2aeb4b
Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>
2022-07-14 16:48:39 +0300

    More advanced and unit test friendly error handling(still crap though..)

--- message_hub.py
+++ message_hub.py
@@ -17,7 +17,7 @@ class message_hub:
         return self.__dict__[method]
 
     def add_extract_method(self, name):
-        construct_str = "def extract_" + name + "(s): return s['" + name + "'][0]"
+        construct_str = "def extract_" + name + "(s): return s['" + name + "']"
 #        print (construct_str)
         exec (construct_str)
 #        print ("self.__dict__['extract_" + name + "'] = extract_" + name)
@@ -25,55 +25,66 @@ class message_hub:
 #        print (self.__dict__)
 
     def handle_request(self, s):
+        err_str = ""
         if not "id" in s:
-            print ("Can't communicate without id!")
-            return ""
+            err_str = "Can't communicate without id!"
+            print (err_str)
+            return err_str
 
         _id = self.extract_id(s)
 
         if "register" in s:
             if _id in self.user_hashes:
-                print ("Such user " + _id + " already registered!")
-                return
+                err_str = "Such user " + _id + " already registered!"
+                print (err_str)
+                return err_str
             if not "pass" in s:
-                print ("No password added!")
-                return
+                err_str = "No password added!"
+                print (err_str)
+                return err_str
             self.user_hashes[_id] = self.extract_pass(s)
 
         if not _id in self.user_hashes:
-            print ("Unregistered user " + _id + "!")
-            return ""
+            err_str = "Unregistered user " + _id + "!"
+            print (err_str)
+            return err_str
 
         if not "hash" in s:
-            print ("Couldn't authenticate without hash!")
-            return ""
+            err_str = "Couldn't authenticate without hash!"
+            print (err_str)
+            return err_str
 
         _hash = self.extract_hash(s)
         if _hash != self.user_hashes[_id]:
-            print ("Wrong hash - not authenticating!")
-            return ""
+            err_str = "Wrong hash - not authenticating!"
+            print (err_str)
+            return err_str
 
         if "change_hash" in s:
             if not "new_hash" in s:
-                print ("No new hash specified!")
-                return ""
+                err_str = "No new hash specified!"
+                print (err_str)
+                return err_str
             new_hash = self.extract_new_hash(s)
             self.user_hashes[_id] = new_hash
 
         if "send" in s:
                 if not "message" in s:
-                    print ("No message field specified!")
-                    return ""
+                    err_str = "No message field specified!"
+                    print (err_str)
+                    return err_str
                 message = self.extract_message(s)
 
                 if not "to" in s:
-                    print ("No 'to' field specified!")
-                    return ""
+                    err_str = "No 'to' field specified!"
+                    print (err_str)
+                    return err_str
                 to = self.extract_to(s)
 
                 if not to in self.user_hashes:
-                    print ("No such recipient!")
-                    return ""
+                    err_str = "No such recipient!"
+                    print (err_str)
+                    return err_str
 
                 if not to in self.mbox:
                     self.mbox[to] = ""
